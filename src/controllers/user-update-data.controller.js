f3ae1a641e45b535c7db000df83351b0defc8950
code17049 <cs17049@ues.edu.sv>
2022-12-03 03:53:29 -0500

    Este es mi primer commit

---
+++ src/controllers/user-update-data.controller.js
@@ -0,0 +1,21 @@
+import UserModel from '#Schemas/user.schema.js';
+const userUpdateDataController = async (req, res) => {
+    const { id } = req;
+    
+    const { name, surname } = req.body;
+    console.log(id);
+
+
+    const existingUserById = await UserModel.findById(id).exec();
+    if (!existingUserById)
+        return res.status(401).send({ errors: ['Usuario no autorizado :('] });
+
+    existingUserById.name = name;
+    existingUserById.surname = surname;
+
+    await existingUserById.save();
+
+    return res.send('Usuario actualizado');
+};
+
+export default userUpdateDataController;
